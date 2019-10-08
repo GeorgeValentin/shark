64c79985e2afa9264a08568cc2f362eabe6be702
xushc <knightflower1989@gmail.com>
2019-10-08 18:41:50 +0800

    init

--- tools/xshell/src/main.rs
+++ tools/xshell/src/main.rs
@@ -12,22 +12,17 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    // Connect to the local SSH server
-    let tcp = TcpStream::connect("10.22.94.247:57522").unwrap();
+    let tcp = TcpStream::connect("192.168.33.20:22").unwrap();
     let mut sess = Session::new().unwrap();
-    sess.handshake(&tcp).unwrap();
-    sess.userauth_password("root", "n743exM87AqruHRu").unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_password("root", "vagrant").unwrap();
 
     let mut channel = sess.channel_session().unwrap();
 
-    eprintln!("requesting pty");
-
     channel.request_pty("xterm", None, None).unwrap();
 
-    eprintln!("shell");
-
     channel.shell().unwrap();
-
 
     let (s1, r1) = crossbeam_channel::unbounded::<String>();
     let (s2, r2) = crossbeam_channel::unbounded::<String>();
@@ -36,56 +31,59 @@ fn main() {
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
             Ok(n) => {
-                println!("{} bytes read", n);
-                println!("{}", input);
-
                 s1.send(input).unwrap();
             }
             Err(error) => println!("error: {}", error),
         }
     });
 
+    let mut stdout = vec![0; 4096];
+    channel.read(&mut stdout).unwrap();
+    println!("{}", String::from_utf8(stdout).unwrap());
+
+    let mut stdout = vec![0; 4096];
+    channel.read(&mut stdout).unwrap();
+    println!("{}", String::from_utf8(stdout).unwrap());
+
     loop {
         select! {
-            recv(r1) -> line1  =>{
-                println!("11111111111");
-                if let Ok(line) = line1{
-                    println!("line1: {}", line);
+                            recv(r1) -> line1  =>{
+                                if let Ok(line) = line1{
 
-                    let cmd_string = line;
-                    channel.write(cmd_string.as_bytes()).unwrap();
+                                    let len = line.len();
 
-                    println!("11111111112");
+                                    let cmd_string = line;
+                                    channel.write(cmd_string.as_bytes()).unwrap();
 
+                                    if channel.exit_status().unwrap() > 0{
 
-                    if channel.exit_status().unwrap() > 0{
+                                        let mut stderr = vec![0; 4096];
+                                        channel.stderr().read(&mut stderr).unwrap();
+                                        println!("stderr: {}", String::from_utf8(stderr).unwrap());
 
-                        let mut stderr = vec![0; 4096];
-                        channel.stderr().read(&mut stderr).unwrap();
-                        println!("stderr: {}", String::from_utf8(stderr).unwrap());
+                                    }
 
-                    }
+                                    let mut stdout = vec![0; 4096];
+                                    channel.read(&mut stdout).unwrap();
+                                    println!(">>{}", String::from_utf8(stdout).unwrap());
 
-                    println!("111111111113");
+                                    if (len>1){
+                                        let mut stdout = vec![0; 4096];
+                                        channel.read(&mut stdout).unwrap();
+                                        println!(">>{}", String::from_utf8(stdout).unwrap());
+                                    }
 
-                    let mut stdout = vec![0; 4096];
-                    channel.read(&mut stdout).unwrap();
+                                }
+                            }
+                            recv(r2) -> line2 => {
+                                if let Ok(line) = line2{
+                                    println!("{}", line);
+                                }
+                            }
+                            default(Duration::from_millis(1000)) => {
 
-                    println!("111111111114");
+                            }
 
-                    println!("stdout: {}", String::from_utf8(stdout).unwrap());
-
-                }
-            }
-            recv(r2) -> line2 => {
-                if let Ok(line) = line2{
-                    println!("line2: {}", line);
-                }
-            }
-//            default(Duration::from_millis(1000)) => {
-//
-//            }
-
-        }
+                        }
     }
 }
