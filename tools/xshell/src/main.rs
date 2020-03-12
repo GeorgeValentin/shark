f23406a9d5d1aad186a92065e16b1a8bab14e244
xushc <knightflower1989@gmail.com>
2020-03-12 18:39:15 +0800

    init

--- tools/xshell/src/main.rs
+++ tools/xshell/src/main.rs
@@ -1,12 +1,18 @@
 extern crate crossbeam;
+extern crate tempfile;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::net::TcpStream as StdTcpStream;
+use std::process::Command;
+use std::thread;
+
+use tempfile::Builder;
 
 use crossbeam::channel::{unbounded, TryRecvError};
 use ssh2::Session;
-use std::io::prelude::*;
-use std::net::TcpStream as StdTcpStream;
-use std::thread;
 
-fn main() {
+fn main_ssh2() {
     let tcp = StdTcpStream::connect("192.168.33.30:22").unwrap();
     let mut sess = Session::new().unwrap();
     sess.set_tcp_stream(tcp);
@@ -62,4 +68,41 @@ fn main() {
             }
         }
     }
+}
+
+fn main() {
+    let username = "root";
+    let password = "vagrant";
+    let ip = "192.168.33.30";
+    let port = "22";
+    let server_name = "vag";
+
+    let mut file = File::open("./ssh.expect").unwrap();
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).unwrap();
+    drop(file);
+
+    let buffer = buffer.replace("__username__", username);
+    let buffer = buffer.replace("__password__", password);
+    let buffer = buffer.replace("__ip__", ip);
+    let buffer = buffer.replace("__port__", port);
+    let buffer = buffer.replace("__server_name__", server_name);
+
+    let tmp_dir = Builder::new().prefix("xshell_rs.").tempdir().unwrap();
+    let file_path = tmp_dir.path().join("ssh.expect");
+
+    let mut file = File::create(file_path.to_str().unwrap()).unwrap();
+    file.write_all(buffer.as_bytes());
+    file.sync_all();
+    drop(file);
+
+    let mut cmd = Command::new("expect");
+
+    cmd.arg("-f").arg(file_path.to_str().unwrap());
+
+    let mut process = cmd.spawn().expect("process failed");
+
+    process.wait().unwrap();
+
+    tmp_dir.close().unwrap();
 }
